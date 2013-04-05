af9657a1c73c6d4e0d706e0f98e9c38a6edf1740
Jon Morehouse <morehousej09@gmail.com>
2013-04-04 21:30:14 -0700

    Update javascript functionality. Embedding yeoman as a replacement for sprockets/asset-pipeline in lieu of better modularization, faster development and easy deployment

--- config/application.rb
+++ config/application.rb
@@ -62,7 +62,7 @@ module Clothing
     config.assets.precompile << /(^[^_\/]|\/[^_])[^\/]*$/
 
     # Enable the asset pipeline
-    config.assets.enabled = true
+    config.assets.enabled = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
--- config/compass.rb
+++ config/compass.rb
@@ -1,9 +1,9 @@
 # Require any additional compass plugins here.
 project_type = :rails
 
-require "susy"
-require "animate-sass"
-require "toolkit"
+require 'compass-rails'
+require 'compass'
+
 
 # Compass.add_configuration({:sprite_load_path => Compass.configuration.sprite_load_path + [Rails.root.join('app', 'assets', 'sprites')]}, 'sprites')
 
