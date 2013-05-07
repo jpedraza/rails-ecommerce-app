d567c4c4e0986ce55f7947caa15d81de93675102
Jon Morehouse <morehousej09@gmail.com>
2013-05-06 22:40:06 -0700

    Update application routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ routes = ["concerns", "shop"]
 # require each of the config files that are need for the configuration
 # we require each element to be loaded in manually so we can manage the order in which files are loaded etc
 # routes.each {|f| require Rails.root.join("config/routes/#{f}.rb")}
-# 
 Clothing::Application.routes.draw do
 
 	root :to => 'home#home'
@@ -12,6 +11,7 @@ Clothing::Application.routes.draw do
 	# initialize shop namespace!
 	namespace :shop do
 
+		# initialize men types etc
 		match "men/:item_type" => "shop#show", :type => "men"
 		match "women/:item_type" => "shop#show", :type => "women"
 		match "kids/:item_type" => "shop#show", :type => "kids"
